3be8fadb08e23d7df4538750f2aa9e65f92fd7b4
Ruan Finamor <75641084+RuanF7@users.noreply.github.com>
2023-01-23 18:26:55 -0300

    Criando tabela

--- index.js
+++ index.js
@@ -13,3 +13,34 @@ function calcHipotenusa() {
 
 
 button.addEventListener('click', calcHipotenusa)
+
+
+var list1 = [];
+var list2 = [];
+var list3 = [];
+
+var n = 1;
+var x = 0;
+
+
+function AddRow() {
+    var AddRow = document.getElementById('show');
+    var NewRow = AddRow.insertRow(n);
+
+    list1[x] = document.getElementById("xInput").value;
+    list2[x] = document.getElementById("yInput").value;
+    list3[x] = Math.round(Math.sqrt((list1 * list1) + (list2 * list2)));
+
+
+
+    var cell1 = NewRow.insertCell(0);
+    var cell2 = NewRow.insertCell(1);
+    var cell3 = NewRow.insertCell(2);
+
+    cell1.innerHTML = list1[x];
+    cell2.innerHTML = list2[x];
+    cell3.innerHTML = list3[x];
+
+    n++;
+    x++;
+}
